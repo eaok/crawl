f1fd6287dfb2994cd931a8af523cd5074d096e86
pi <kcoewoys@gmail.com>
2016-11-21 18:51:44 +0000

    代码优化

--- qiantu/qiantu/pipelines.py
+++ qiantu/qiantu/pipelines.py
@@ -7,14 +7,14 @@
 
 import re
 import os
-import random
 import urllib.request
 
 
 class QiantuPipeline(object):
     def process_item(self, item, spider):
-        if os.path.exists('./output') == False:
-            os.mkdir('./output')
+        path = 'output'
+        if not os.path.exists(path):
+            os.mkdir(path)
 
         for i in range(0, len(item["url"])):
             try:
@@ -23,15 +23,13 @@ class QiantuPipeline(object):
                 id = re.compile(pat).findall(thisurl)
                 thistrueurl = "http://pic.qiantucdn.com/58pic/" + id[0] + "_1024.jpg"
 
-                if os.path.exists('./output/' + item["folder_name"]) == False:
-                    os.mkdir('./output/' + item["folder_name"])
+                if not os.path.exists(path + '/' +item["folder_name"]):
+                    os.mkdir(path + '/' + item["folder_name"])
 
-#                避免名字重复，名字再加上个随机数
-#                file="./output/"+id[-7:]+str(int(random.random()*10000))+".jpg"
-                file = "./output/" + item['folder_name'] + "/" + thistrueurl.split('/')[-1]
+                file = path + '/' + item['folder_name'] + '/' + thistrueurl.split('/')[-1]
                 print(file)
 
-                urllib.request.urlretrieve(thistrueurl, file)
+#                urllib.request.urlretrieve(thistrueurl, file)
 
             except Exception as e:
                 pass
